801cf164e0552ae1d0f9d77beb929ff237418b9f
Jaydenho99 <khumleon31@student.usm.my>
2023-05-18 23:48:10 +0800

    Edit and display search result to UI

--- app.py
+++ app.py
@@ -2,15 +2,30 @@ from flask import Flask, render_template, request, jsonify, make_response, sessi
 from urllib.request import urlopen
 import simplejson
 import pysolr
+import time
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
-solr = pysolr.Solr('http://localhost:8983/solr')
+solr = pysolr.Solr('http://localhost:8983/solr/searchengine', timeout=10)
+if(solr):
+    print("Successfully connect to SoLR")
+else:
+    print("Failed to connect to SoLR")
 
 
-@app.route('/home')
+@app.route('/home', methods=["POST","GET"])
 def home():
-    return render_template('index.html')
+
+    if request.args.get('search-btn'):
+        
+        query=request.args.get('search')
+        start_time = time.time()  # Start measuring time
+        results=solr.search(query)
+        end_time = time.time()  # End measuring time
+        num_found=results.hits
+        response_time = round(end_time - start_time,3)  # Calculate the response time
+        
+    return render_template('index.html',query=query,results=results,num_found=num_found,response_time=response_time)
 
 
 if __name__ == "__main__":
