071e845e61c21b291efe36714eab3eedf7ae6927
Jaydenho99 <khumleon31@student.usm.my>
2023-05-18 22:10:26 +0800

    add library

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, jsonify, make_response, session, url_for, redirect, flash, Markup
 from urllib.request import urlopen
 import simplejson
+import pysolr
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
+solr = pysolr.Solr('http://localhost:8983/solr')
+
 
 @app.route('/home')
 def home():
