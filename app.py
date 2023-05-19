96f7c192ac4cb9a51fa3a2701b5e6b4bd965d91d
Jaydenho99 <khumleon31@student.usm.my>
2023-05-19 16:39:38 +0800

    Done pagination

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from urllib.request import urlopen
 import simplejson
 import pysolr
 import time
+from flask_paginate import Pagination, get_page_args
+
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
@@ -20,11 +22,43 @@ def home():
         
         query=request.args.get('search')
         start_time = time.time()  # Start measuring time
-        results=solr.search(query,start=0,rows=40)
+        results=solr.search(query,start=0,rows=200)
         end_time = time.time()  # End measuring time
+
+
+        per_page = 10
+        page = int(request.args.get('page', 1))
+        # Retrieve total number of items in collection
         num_found=results.hits
+
+        # Calculate the start and end index for the current page
+        start_index = (page - 1) * per_page
+        end_index = start_index + per_page
+
+        # Extract values from the Results object
+        formatted_results = []
+        for result in results:
+            formatted_result = {
+                'title': str(result['title'][0]) if 'title' in result else '',
+                'link': str(result['link'][0]) if 'link' in result else '',
+                'description': str(result['description'][0]) if 'description' in result else '',
+                'pubDate': str(result['pubDate'][0]) if 'pubDate' in result else '',
+                'source_id': str(result['source_id'][0]) if 'source_id' in result else '',
+                'country': str(result['country'][0]) if 'country' in result else ''
+            }
+            formatted_results.append(formatted_result)
+
+        # Retrieve items for the current page using list slicing
+        pagination_results = formatted_results[start_index:end_index]
+
+        # Create pagination object using Flask-Paginate
+        pagination = Pagination(page=page, per_page=per_page,
+                                    total=num_found, css_framework='bootstrap4')
+        
+        
+        
         response_time = round(end_time - start_time,3)  # Calculate the response time
-        return render_template('index.html',query=query,results=results,num_found=num_found,response_time=response_time)
+        return render_template('index.html',query=query,results=pagination_results ,pagination=pagination,num_found=num_found,response_time=response_time)
     return render_template('index.html')
 
 
